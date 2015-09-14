ab538522f6b5ccbd7554354071f73610141e2458
Alex Chan <alex415@gmail.com>
2015-09-14 13:58:39 -0700

    ready for heroku deploy

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ function redirectUnmatched(req, res) {
   res.redirect('/');
 }
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('server started on localhost:3000');
 });
